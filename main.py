e6d70d177270db9775161951b1cbc1b7b027745f
Kowwok321 <t.maso321@gmail.com>
2026-09-02 14:41:24 -0400

    added some comments to the expensetracker functions

---
+++ main.py
@@ -0,0 +1,32 @@
+class expensetracker:
+    def __init__(self):
+        self.expenses
+        self.categories
+        self.income
+
+def add_expense(self, amount, date, category):
+#need to add the expense to the expenses list based on the user's input for amount, date, and category
+
+
+def add_category(self, category):
+#need to add the category to the categories list based on the user's input for category
+
+
+def add_income(self, amount, date_range):
+#need to add the income to the income list based on the user's input for amount and date_range
+
+
+def list_expenses(self):
+#need to print the expenses list to the user
+
+
+def list_categories(self):
+#need to print the categories list to the user
+
+
+
+
+
+
+
+main ()
